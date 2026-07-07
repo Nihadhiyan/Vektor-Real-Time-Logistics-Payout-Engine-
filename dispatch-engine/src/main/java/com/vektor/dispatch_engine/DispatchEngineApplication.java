0daa190b3da2cff7577407ecc94429b6ac537c70
Mohamed Nihadh <nihath854@gmail.com>
2026-07-07 05:48:44 +0530

    refactor: restructure repo and migrate to Apache Kafka
    Restructured project into a monorepo format for Python and Java modules. Migrated docker-compose.yml to the official Apache Kafka image using standalone KRaft mode.

---
+++ dispatch-engine/src/main/java/com/vektor/dispatch_engine/DispatchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.vektor.dispatch_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchEngineApplication.class, args);
+	}
+
+}
---
+++ dispatch-engine/src/test/java/com/vektor/dispatch_engine/DispatchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vektor.dispatch_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
